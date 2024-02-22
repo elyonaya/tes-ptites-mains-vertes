9d95c7eff70e9a714c9caad006376c40cbf076a3
amel fradj <amelfradj1@gmail.com>
2024-02-22 22:57:31 +0100

    @m

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
 });
+
+document.querySelector('.burger-menu').addEventListener('click', function() {
+  this.classList.toggle('open');
+  document.querySelector('.menu').classList.toggle('open');
+  var menuLinks = document.querySelectorAll('.menu ul li');
+  menuLinks.forEach(function(link) {
+    link.classList.toggle('fade');
+  });
+});
